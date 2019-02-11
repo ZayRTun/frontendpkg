a078ce6b7a2086e33ec231510187d1bcabb29ac5
Zayar Tun <zay.t@hotmail.com>
2019-02-11 23:15:09 +0630

    VueRouter added and optimized spa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,31 @@
 // Vue
 window.Vue = require('vue');
 
+window.Event = new Vue();
+
 // Vue Component
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter)
+
 import ExampleComponent from './components/ExampleComponent.vue';
-import navbar from './components/NavbarComponent.vue';
+import NavbarComponent from './components/NavbarComponent.vue';
+import HomeComponent from './components/HomeComponent.vue';
+import AboutComponent from './components/AboutComponent.vue';
+
+const routes = [
+    { path: '/', component: HomeComponent},
+    { path: '/about', component: AboutComponent}
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
 
 const app = new Vue({
     el: '#app',
-    components: { ExampleComponent, navbar }
+    components: { 'navbar-component': NavbarComponent},
+    router
 });
---
+++ resources/js/components/AboutComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="section">
+        <div class="container">
+            <h1 class="title is-1">About Page</h1>
+            <p class="content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima hic, rem amet sit ab modi beatae. Accusamus consequuntur sapiente reiciendis asperiores alias magnam esse debitis numquam, nihil, vel sed nisi, voluptas nostrum blanditiis harum. Voluptate aut iste, excepturi ratione recusandae tenetur veniam aperiam quae vitae illum placeat hic a laborum.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
